a3e25458ea0aa40e53d523373a9c21f0a89753d1
Luca Calcaterra <calcaterra.luca@gmail.com>
2023-04-11 19:27:50 +0200

    fix laravel version and table name

--- src/Models/City.php
+++ src/Models/City.php
@@ -3,15 +3,15 @@
 namespace LucaCalcaterra\FilamentItalianCities\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class City extends Model
 {
     use HasFactory;
 
-    protected $table = 'cities';
+    protected $table = 'italy_cities';
 
     public function province(): BelongsTo
     {
--- src/Models/Province.php
+++ src/Models/Province.php
@@ -2,16 +2,16 @@
 
 namespace LucaCalcaterra\FilamentItalianCities\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Province extends Model
 {
     use HasFactory;
 
-    protected $table = 'provinces';
+    protected $table = 'italy_provinces';
 
     public function cities(): HasMany
     {
